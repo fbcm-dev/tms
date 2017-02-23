ad8b6a68ddf55e2b5b9d8d5c1bcb343acc3ca21e
fenderHartke <jeielaraneta@gmail.com>
2017-02-23 23:26:30 +0800

    add migrations

--- database/migrations/2017_02_23_132540_create_members_table.php
+++ database/migrations/2017_02_23_132540_create_members_table.php
@@ -16,20 +16,15 @@ class CreateMembersTable extends Migration
         Schema::create('members', function (Blueprint $table) {
             $table->increments('id');
             $table->string('first_name', 45);
-            $table->string('middle_name', 45);
-            $table->string('last_name', 45);
-            $table->string('address', 100);
-            $table->date('birthday');
-            $table->integer('age');
-            $table->string('email', 45);
-            $table->mediumText('contacts');
             $table->string('organization', 45);
             $table->integer('created_by')->unsigned();
 
             $table->foreign('created_by')
                 ->references('id')
                 ->on('users')
-                ->onDelete('cascade');
+                ->onDelete('cascade')
+                ->nullable()
+                ->change();
 
             $table->timestamps();
         });
