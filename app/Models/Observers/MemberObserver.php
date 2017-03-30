bb38aca34be18b6b106a47f86ee3164699e32735
fenderHartke <jeielaraneta@gmail.com>
2017-03-30 23:41:25 +0800

    Added Member Observer

---
+++ app/Models/Observers/MemberObserver.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace TMS\Models\Observers;
+
+use TMS\Models\Member;
+
+/**
+ * Class MemberObserver
+ * @package TMS\Models\Observers
+ *
+ * listens to Member Model
+ */
+class MemberObserver
+{
+    public function created(Member $member){}
+
+    public function creating(Member $member){
+       $member->setCodeAttribute();
+       $member->setCreatedByAttribute();
+    }
+}
